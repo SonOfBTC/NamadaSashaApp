c78f5f54dad548d414983064af3e5e1728b59caf
SonOfBTC <96632062+SonOfBTC@users.noreply.github.com>
2024-03-15 15:39:51 +0300

    Update index.js
    P2 Game added

--- index.js
+++ index.js
@@ -3,12 +3,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { exec } = require('child_process');
+const path = require('path');
+const cors = require('cors');
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
@@ -17,7 +20,7 @@ app.get('/', (req, res) => {
 app.post('/submit', (req, res) => {
     const targetAddress = req.body.targetAddress;
 
-    const command = `namadac transfer --source Yourwalletallias --target ${targetAddress} --token naan --amount 0.00001 --memo yourpublickey --node http://namadasasharpc.xyz/`;
+    const command = `namadac transfer --source YourWalletalias --target ${targetAddress} --token naan --amount 0.1 --memo YourPublicKey --node http://namadasasharpc.xyz/`;
 
     exec(command, (error, stdout, stderr) => {
         if (error) {
@@ -104,6 +107,31 @@ app.get('/lastBlock', (req, res) => {
     });
 });
 
-app.listen(port, () => {
+app.get('/snake', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index2.html'));
+});
+
+app.post('/submit-score', (req, res) => {
+    const wallet = req.body.wallet;
+    const playerScore = req.body.playerScore;
+
+    // Skor ve oyuncu bilgilerini kullanarak transfer işlemini gerçekleştir
+    const command = `namadac transfer --amount ${playerScore} --source Yourwalletalias --target ${wallet} --token naan --memo YourPublicKey`;
+
+    exec(command, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`Hata oluştu: ${error.message}`);
+            res.json({ message: `Hata oluştu: ${error.message}` });
+            return;
+        }
+
+        console.log(`Transfer işlemi başarıyla tamamlandı. Çıktı: ${stdout}`);
+        
+        // İşlem başarıyla tamamlandıysa, isteği yapan tarayıcıya başarı mesajını gönder
+        res.json({ message: 'Transfer completed successfully' });
+    });
+});
+
+app.listen(port, '0.0.0.0', () => {
     console.log(`Server http://localhost:${port} adresinde çalışıyor`);
 });
